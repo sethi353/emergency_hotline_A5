8221d21f7790f7f357ef845ab841ef3683f5b728
sethi353 <sethisaha56@gmail.com>
2025-08-29 08:36:54 +0600

    updated index.html file

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const services = [
   { id: 1, name: "National Emergency Number", nameEn: "National Emergency", number: "999", category: "Emergency", icon: "./assets/emergency.png" },
   { id: 2, name: "Police Helpline Number", nameEn: "Police", number: "999", category: "Emergency", icon: "./assets/police.png" },
   { id: 3, name: "Fire Service Number", nameEn: "Fire Service", number: "999", category: "Emergency", icon: "icons/police.png" },
-  { id: 4, name: "Gas Help", nameEn: "Gas", number: "104", category: "Utility", icon: "icons/gas.png" },
+  { id: 4, name: "Ambulence Service", nameEn: "Ambulence", number: "1994-9999999", category: "Emergency", icon: "./assets/ambulance.png" },
   { id: 5, name: "Electricity", nameEn: "Electric", number: "105", category: "Utility", icon: "icons/electric.png" },
   { id: 6, name: "Water Help", nameEn: "Water", number: "106", category: "Utility", icon: "icons/water.png" },
 ];
